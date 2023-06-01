7204c774cbd7e92c2a9aacb4375195a2d3b5a8b8
Rares <rares.andrei.tabacaru@gmail.com>
2023-06-01 12:34:00 +0300

    [Update] Added app configurations for authentication in Program.cs

--- DAWM Project/DAWM Project/Program.cs
+++ DAWM Project/DAWM Project/Program.cs
@@ -1,6 +1,9 @@
 using DAWM_Project.Settings;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using RestaurantAPI.Domain;
 using RestaurantAPI.Logger;
+using System.Text;
 
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "Log4Net.config", Watch = true)]
 
@@ -19,6 +22,31 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IDataLogger, Logger>();
 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ClockSkew = TimeSpan.Zero,
+
+        ValidIssuer = "LabAPI-Backend",
+        ValidAudience = "LabAPI-Anyone",
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:SecurityKey"]))
+    };
+});
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -29,6 +57,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
