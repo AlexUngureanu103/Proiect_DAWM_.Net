8bf8f84a17925e80c532932c802dace317e57c9b
Rares <rares.andrei.tabacaru@gmail.com>
2023-06-01 12:21:21 +0300

    [Update] Added UsersService and AccountController

---
+++ DAWM Project/Core/Services/UsersService.cs
@@ -0,0 +1,21 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services
+{
+    public class UsersService
+    {
+        private readonly UnitOfWork unitOfWork;
+
+        private AuthorizationService authService { get; set; }
+        public UsersService(UnitOfWork unitOfWork, AuthorizationService authService)
+        {
+            this.unitOfWork = unitOfWork;
+            this.authService = authService;
+        }
+    }
+}
---
+++ DAWM Project/DAWM Project/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DAWM_Project.Controllers
+{
+    [ApiController]
+    [Route("api/account")]
+    public class AccountController : ControllerBase
+    {
+        private UsersService UsersService { get; set; }
+
+        public AccountController(UsersService usersService)
+        {
+            this.UsersService = usersService;
+        }
+
+        [HttpPost("/register")]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return Ok();
+        }
+
+        [HttpPost("/login")]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return Ok();
+        }
+    }
+}
