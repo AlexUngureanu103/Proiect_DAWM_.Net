ec640dd0208c404c5e910382f39982b098c035e2
Rares <rares.andrei.tabacaru@gmail.com>
2023-06-09 13:27:06 +0300

    [Update] Added interfaces and dto for Menu

---
+++ DAWM Project/RestaurantAPI.Domain/Dtos/CreateOrUpdateMenu.cs
@@ -0,0 +1,11 @@
+﻿using RestaurantAPI.Domain.Models.MenuRelated;
+
+namespace RestaurantAPI.Domain.Dtos
+{
+    public class CreateOrUpdateMenu
+    {
+        public string Name { get; set; }
+
+        public float Price { get; set; }
+    }
+}
---
+++ DAWM Project/RestaurantAPI.Domain/ServicesAbstractions/IMenusService.cs
@@ -0,0 +1,19 @@
+﻿using RestaurantAPI.Domain.Dtos;
+using RestaurantAPI.Domain.Dtos.DishesTypeDtos;
+using RestaurantAPI.Domain.Models.MenuRelated;
+
+namespace RestaurantAPI.Domain.ServicesAbstractions
+{
+    public interface IMenusService
+    {
+        Task<bool> AddMenu(CreateOrUpdateMenu menu);
+
+        Task<bool> DeleteMenu(int menuId);
+
+        Task<bool> UpateMenu(int menuId, CreateOrUpdateMenu menu);
+
+        Task<Menu> GetMenuById(int menu);
+
+        Task<IEnumerable<Menu>> GetAllMenus();
+    }
+}
