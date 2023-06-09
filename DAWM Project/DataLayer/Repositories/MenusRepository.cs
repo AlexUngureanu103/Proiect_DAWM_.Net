eb6d0f011b08ad6e60df2e35117ef255e9898ffd
Rares <rares.andrei.tabacaru@gmail.com>
2023-06-09 13:29:14 +0300

    [Update] Added implementations for menus service and repository + mapping for dto

---
+++ DAWM Project/DataLayer/Repositories/MenusRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantAPI.Domain.Models.MenuRelated;
+using RestaurantAPI.Domain.RepositoriesAbstractions;
+using RestaurantAPI.Exceptions;
+using System.Linq;
+
+namespace DataLayer.Repositories
+{
+    public class MenusRepository : RepositoryBase<Menu>, IMenusRepository
+    {
+        private readonly DbSet<Menu> _dbSet;
+        public MenusRepository(AppDbContext dbContext) : base(dbContext)
+        {
+            dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+            _dbSet = dbContext.Set<Menu>();
+        }
+
+        public new async Task UpdateAsync(int entityId, Menu entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            var entityFromDb = await GetByIdAsync(entityId);
+            if (entityFromDb == null)
+                throw new EntityNotFoundException($"{nameof(Recipe)} with id {entity.Id} does not exist.");
+
+            entityFromDb.Name = entity.Name;
+            entityFromDb.Price = entity.Price;
+        }
+    }
+}
---
+++ DAWM Project/RestaurantAPI.Domain/Mapping/MenuMapping.cs
@@ -0,0 +1,26 @@
+﻿using RestaurantAPI.Domain.Dtos;
+using RestaurantAPI.Domain.Dtos.DishesTypeDtos;
+using RestaurantAPI.Domain.Models.MenuRelated;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Domain.Mapping
+{
+    public static class MenuMapping
+    {
+        public static Menu MapToMenu(CreateOrUpdateMenu menu)
+        {
+            if (menu == null)
+                return null;
+
+            return new Menu
+            {
+                Name = menu.Name,
+                Price = menu.Price,
+            };
+        }
+    }
+}
